cea3881da55b27cf263be62553eedf4c97a251a4
alangford <a.b.l.2142@gmail.com>
2018-01-17 22:34:14 -0700

    fixed links

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -23,19 +23,19 @@ const Projects = () => [
       <img id="SEEK" src={seek}/>
     </a-assets>,
     <Entity text={{value: 'Adobe Clone', height:"5", width:"5", align: 'center'}} rotation='0 0 0' position={{x: -3, y: 2, z: -9}}/> ,
-    <a-image href='https://adobe.andrewlangford.net' src="#ADOBE" position='-3 1 -9.5' />,
+    <a-image href='http://adobe.andrewlangford.net' src="#ADOBE" position='-3 1 -9.5' />,
     <Entity text={{value: 'BoardGameShelf.net', height:"5", width:"5", align: 'center'}} rotation='0 0 0' position={{x: -1, y: 2, z: -9}}/> ,
     <a-image href='https://www.boardgameshelf.net' src="#BGS" position='-1 1 -9.5' />,
     <Entity text={{value: 'Pong in JS', height:"5", width:"5", align: 'center'}} rotation='0 0 0' position={{x: 1, y: 2, z: -9}}/> ,
-    <a-image href='https://pong.andrewlangford.net' src="#PONG" position='1 1 -9.5' />,
+    <a-image href='http://pong.andrewlangford.net' src="#PONG" position='1 1 -9.5' />,
     <Entity text={{value: 'A Heat Map App', height:"5", width:"5", align: 'center'}} rotation='0 0 0' position={{x: 3, y: 2, z: -9}}/> ,
-    <a-image href='https://seek.andrewlangford.net' src="#SEEK" position='3 1 -9.5' />,
+    <a-image href='http://seek.andrewlangford.net' src="#SEEK" position='3 1 -9.5' />,
     <Entity text={{value: 'A React-Native App', height:"5", width:"5", align: 'center'}} rotation='0 0 0' position={{x: -3, y: 5, z: -9}}/> ,
     <a-image href='https://github.com/Dream-Team99/On-The-Line-app' src="#OTL" position='-3 4 -9.5' />,
     <Entity text={{value: 'Tetris in JS', height:"5", width:"5", align: 'center'}} rotation='0 0 0' position={{x: 0, y: 5, z: -9}}/> ,
-    <a-image href='https://tetris.andrewlangford.net' src="#TETRIS" position='0 4 -9.5' />,
+    <a-image href='http://tetris.andrewlangford.net' src="#TETRIS" position='0 4 -9.5' />,
     <Entity text={{value: 'w3schools clone', height:"5", width:"5", align: 'center'}} rotation='0 0 0' position={{x: 3, y: 5, z: -9}}/> ,
-    <a-image href='https://w3clone.andrewlangford.net' src="#W3" position='3 4 -9.5' />,
+    <a-image href='http://w3clone.andrewlangford.net' src="#W3" position='3 4 -9.5' />,
 
 ];
 
